ec8b73a713c4a8affe87771aa8a1c68adb8872f1
wanghan <wanghan1@eastmoney.com>
2020-03-03 16:38:13 +0800

    删除冗余页面

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -2,30 +2,20 @@
   <div>
     <el-menu
       :default-active="activeIndex2"
-      style="padding-left: 100px"
+      style="padding-left: 20px"
       class="el-menu-demo"
       mode="horizontal"
       @select="handleSelect"
       background-color="#545c64"
       text-color="#fff"
       active-text-color="#ffd04b">
-      <!--<el-menu-item index="/home">首页</el-menu-item>-->
+      <el-menu-item disabled index="/home">
+        <span class="systemName">APP智能监控平台</span>
+      </el-menu-item>
       <el-menu-item index="/indexMonitor">APP指标监控</el-menu-item>
       <el-menu-item index="/updateMonitor">APP升级监控</el-menu-item>
       <el-menu-item index="/appManage">应用管理</el-menu-item>
       <el-menu-item index="/admin">我的工作台</el-menu-item>
-      <!--<el-submenu index="2">-->
-        <!--<template slot="title">我的工作台</template>-->
-        <!--<el-menu-item index="2-1">选项1</el-menu-item>-->
-        <!--<el-menu-item index="2-2">选项2</el-menu-item>-->
-        <!--<el-menu-item index="2-3">选项3</el-menu-item>-->
-        <!--<el-submenu index="2-4">-->
-          <!--<template slot="title">选项4</template>-->
-          <!--<el-menu-item index="2-4-1">选项1</el-menu-item>-->
-          <!--<el-menu-item index="2-4-2">选项2</el-menu-item>-->
-          <!--<el-menu-item index="2-4-3">选项3</el-menu-item>-->
-        <!--</el-submenu>-->
-      <!--</el-submenu>-->
     </el-menu>
   </div>
 </template>
@@ -46,3 +36,15 @@
     }
   }
 </script>
+<style scoped>
+ .systemName{
+   display: inline-block;
+   margin-right: 100px;
+   margin-left: 0;
+   font-size: 20px;
+   color: rgb(255, 208, 75) !important;
+ }
+  .el-menu-item.is-disabled{
+    opacity: 1 !important;
+  }
+</style>
--- src/components/pages/updateMonitor.vue
+++ src/components/pages/updateMonitor.vue
@@ -1,24 +1,21 @@
 <!--created by wanghan-->
 <template>
-  <div>
+  <div class="main">
     <div>
 
-      <line-chart :trendData="trendData"></line-chart>
+
     </div>
   </div>
 </template>
 
 <script type="text/javascript">
-  import lineChart from "./lineChart";
 
   export default {
     name: '',
     components: {
-      lineChart
     },
     data() {
       return {
-        trendData:[820, 932, 901, 934, 1290, 1330, 1320]
       }
     },
     computed: {
